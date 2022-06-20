c71a2be3bc812dc8493ea93b79820657eab148f8
yujeong lim <b.imnuj@gmail.com>
2022-06-20 23:56:56 +0900

    init

---
+++ src/main/java/shop/tryit/TryitApplication.java
@@ -0,0 +1,13 @@
+package shop.tryit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/tryit/TryitApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.tryit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
