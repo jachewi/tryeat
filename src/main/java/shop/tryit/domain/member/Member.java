f6b6a4f9e8209fd5be248e7c0e06163bc190a752
JungHyun Kim <93319948+Kimjunghyunn@users.noreply.github.com>
2022-06-22 22:30:29 +0900

    feat: 회원 엔티티 클래스,회원 레파지토리 생성 (#13)
    * feat: 회원 엔티티 클래스 생성
    
    * feat: 주소 임베디드 타입 생성
    
    * feat: 회원 권한 ENUM 생성
    
    * feat: 회원 레파지토리 생성
    
    * feat: 회원 레파지토리 테스트(회원가입, 회원존재여부)
    
    * refactor: 회원 엔티티 Setter 삭제
    
    Co-authored-by: Kimjunghyunn <wjdgus6846@gmail.com>

---
+++ src/main/java/shop/tryit/domain/member/Member.java
@@ -0,0 +1,56 @@
+package shop.tryit.domain.member;
+
+import static javax.persistence.EnumType.STRING;
+import static lombok.AccessLevel.PROTECTED;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.tryit.domain.common.Address;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    private String phoneNumber;
+    private String password;
+
+    @Enumerated(STRING)
+    private MemberRole role;
+
+    @Builder
+    private Member(String email,
+                   String name,
+                   Address address,
+                   String phoneNumber,
+                   String password,
+                   MemberRole role) {
+        this.email = email;
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.role = role;
+    }
+
+}
