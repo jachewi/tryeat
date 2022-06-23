245b652ee9b92a2e54695dd4fbcb228c1e625e95
kureung <89060425+kureung@users.noreply.github.com>
2022-06-23 21:22:56 +0900

    feat: QnA 게시판 엔티티 생성 (#19)
    * feat: 질문 엔티티 구현
    
    * feat: 답변 엔티티 구현
    
    * refactor: 질문 엔티티에 기본 생성자 추가
    
    * feat: 답변 엔티티에 수정 기능 구현

---
+++ src/main/java/shop/tryit/domain/question/Question.java
@@ -0,0 +1,57 @@
+package shop.tryit.domain.question;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.tryit.domain.common.BaseTimeEntity;
+import shop.tryit.domain.member.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Question extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private Question(String title, String content, Member member) {
+        this.title = title;
+        this.content = content;
+        this.member = member;
+    }
+
+    public static Question of(String title, String content, Member member) {
+        return new Question(title, content, member);
+    }
+
+    public void update(String title, String content) {
+        changeTitle(title);
+        changeContent(content);
+    }
+
+    private void changeTitle(String title) {
+        this.title = title;
+    }
+
+    private void changeContent(String content) {
+        this.content = content;
+    }
+
+}
