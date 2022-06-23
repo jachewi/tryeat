fbf934d5d4bdcfb9b7141cb30b81e3a67c91d1d8
kureungkureung <terry9611@naver.com>
2022-06-23 20:13:59 +0900

    feat: 질문 리포지토리 구현체 생성

---
+++ src/main/java/shop/tryit/repository/question/QuestionRepositoryImpl.java
@@ -0,0 +1,8 @@
+package shop.tryit.repository.question;
+
+import org.springframework.stereotype.Repository;
+import shop.tryit.domain.question.QuestionRepository;
+
+@Repository
+public class QuestionRepositoryImpl implements QuestionRepository {
+}
