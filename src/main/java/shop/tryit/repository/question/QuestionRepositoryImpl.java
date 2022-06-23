af83cf25b18cae30aff51d9088ef480c2371d0a5
kureungkureung <terry9611@naver.com>
2022-06-23 20:19:01 +0900

    feat: 질문 리포지토리 구현체에 스프링 데이터 리포지토리 의존성 추가

--- src/main/java/shop/tryit/repository/question/QuestionRepositoryImpl.java
+++ src/main/java/shop/tryit/repository/question/QuestionRepositoryImpl.java
@@ -1,8 +1,13 @@
 package shop.tryit.repository.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import shop.tryit.domain.question.QuestionRepository;
 
 @Repository
+@RequiredArgsConstructor
 public class QuestionRepositoryImpl implements QuestionRepository {
+
+    private final QuestionJpaRepository jpaRepository;
+
 }
