e1afa2743a78d7b21a9cbd49ed3c745ed596b495
yujeong lim <b.imnuj@gmail.com>
2022-06-24 15:54:33 +0900

    feat: 상품 등록 기능 개발

---
+++ src/main/java/shop/tryit/domain/item/ItemService.java
@@ -0,0 +1,22 @@
+package shop.tryit.domain.item;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 상품 등록
+     */
+    @Transactional
+    public Long register(Item item) {
+        return itemRepository.save(item);
+    }
+
+}
