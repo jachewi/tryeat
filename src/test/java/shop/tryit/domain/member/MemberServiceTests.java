f011ab423c1faf55f96d2c71f499f6a18497254c
Kimjunghyunn <wjdgus6846@gmail.com>
2022-06-24 12:57:22 +0900

    refactor: 회원 서비스 테스트에서 MemberFormDto 의존성 삭제

--- src/test/java/shop/tryit/domain/member/MemberServiceTests.java
+++ src/test/java/shop/tryit/domain/member/MemberServiceTests.java
@@ -1,12 +1,11 @@
 package shop.tryit.domain.member;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -16,21 +15,13 @@ class MemberServiceTest {
     @Autowired
     MemberService memberService;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    public Member createMember() {
-        MemberFormDto memberFormDto = MemberFormDto.builder()
-                .email("test@email.com")
-                .name("테스트")
-                .password("1111")
-                .build();
-        return Member.createMember(memberFormDto, passwordEncoder);
-    }
-
     @Test
     void 회원가입_테스트() {
-        Member member = createMember();
+        Member member = Member.builder()
+                .email("test1234@gmail.com")
+                .name("test")
+                .password("11111")
+                .build();
         Member savedMember = memberService.saveMember(member);
 
         assertEquals(member.getEmail(), savedMember.getEmail());
@@ -40,16 +31,24 @@ class MemberServiceTest {
     }
 
     @Test
-    void 중복_회원_테스트() {
-        Member member1 = createMember();
-        Member member2 = createMember();
+    void 중복_회원_성공_테스트() {
+        Member member1 = Member.builder()
+                .email("test1234@gmail.com")
+                .name("test")
+                .password("11111")
+                .build();
+
+        Member member2 = Member.builder()
+                .email("test1234@gmail.com")
+                .name("test")
+                .password("11111")
+                .build();
+
         memberService.saveMember(member1);
 
-        Throwable e = assertThrows(IllegalStateException.class, () -> {
-            memberService.saveMember(member2);
-        });
-
-        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+        assertThatThrownBy(() -> memberService.saveMember(member2))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("이미 가입된 회원입니다.");
     }
 
 }
