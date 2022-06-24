77663e12d571eb9ad21acf1c426f9bd3ed258ec7
kureung <89060425+kureung@users.noreply.github.com>
2022-06-24 17:11:53 +0900

    feat: 게시판 조회 기능 구현 (#35)
    * feat: 질문 리포지토리에서 조회 기능 구현
    
    * feat: 질문 서비스에서 조회 기능 구현

--- src/main/java/shop/tryit/domain/question/QuestionRepository.java
+++ src/main/java/shop/tryit/domain/question/QuestionRepository.java
@@ -1,7 +1,11 @@
 package shop.tryit.domain.question;
 
+import java.util.Optional;
+
 public interface QuestionRepository {
 
     Long save(Question question);
+
+    Optional<Question> findById(Long id);
 
 }
