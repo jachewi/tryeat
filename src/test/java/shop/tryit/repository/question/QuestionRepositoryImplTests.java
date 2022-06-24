e6867f8a33c84740d22d20668ab7d80310fb4b77
kureungkureung <terry9611@naver.com>
2022-06-24 13:59:14 +0900

    refactor: 스프링 통합 테스트로 변경

--- src/test/java/shop/tryit/repository/question/QuestionRepositoryImplTests.java
+++ src/test/java/shop/tryit/repository/question/QuestionRepositoryImplTests.java
@@ -4,12 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import shop.tryit.domain.member.Member;
 import shop.tryit.domain.question.Question;
 import shop.tryit.domain.question.QuestionRepository;
 
-@DataJpaTest
+@SpringBootTest
+@Transactional
 class QuestionRepositoryImplTests {
 
     @Autowired
