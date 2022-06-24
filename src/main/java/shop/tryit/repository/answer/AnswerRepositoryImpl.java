c2536ba0b831d77ea309c002af59da2e9b3c9573
kureungkureung <terry9611@naver.com>
2022-06-24 16:52:04 +0900

    feat: 답변 리포지토리에 JPA 리포지토리 의존성 추가

--- src/main/java/shop/tryit/repository/answer/AnswerRepositoryImpl.java
+++ src/main/java/shop/tryit/repository/answer/AnswerRepositoryImpl.java
@@ -1,8 +1,13 @@
 package shop.tryit.repository.answer;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import shop.tryit.domain.answer.AnswerRepository;
 
 @Repository
+@RequiredArgsConstructor
 public class AnswerRepositoryImpl implements AnswerRepository {
+
+    private final AnswerJpaRepository jpaRepository;
+
 }
