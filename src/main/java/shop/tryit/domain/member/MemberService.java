ab8a22ffadcf6ab0ca262bbbaf1cb8f1543d025d
Kimjunghyunn <wjdgus6846@gmail.com>
2022-06-24 12:53:25 +0900

    refactor: saveMember 메소드에 비밀번호 암호화 로직 추가

--- src/main/java/shop/tryit/domain/member/MemberService.java
+++ src/main/java/shop/tryit/domain/member/MemberService.java
@@ -1,6 +1,7 @@
 package shop.tryit.domain.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.tryit.repository.member.MemberJpaRepository;
@@ -11,15 +12,17 @@ import shop.tryit.repository.member.MemberJpaRepository;
 public class MemberService {
 
     private final MemberJpaRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member saveMember(Member member) {
         validateDuplicateMember(member);
+        String encodedPassword = passwordEncoder.encode(member.getPassword());
+        member.builder().password(encodedPassword).build();
         return repository.save(member);
     }
 
     private void validateDuplicateMember(Member member) {
-        boolean findMember = repository.existsByEmail(member.getEmail());
-        if (findMember != false) {
+        if (repository.existsByEmail(member.getEmail())) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
     }
