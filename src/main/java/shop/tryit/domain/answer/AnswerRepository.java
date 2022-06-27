4b4b888a129a5dc66698425100f0662c270c3d05
kureungkureung <terry9611@naver.com>
2022-06-27 21:31:44 +0900

    feat: 질문에 달려있는 답글 조회 기능 구현

--- src/main/java/shop/tryit/domain/answer/AnswerRepository.java
+++ src/main/java/shop/tryit/domain/answer/AnswerRepository.java
@@ -1,11 +1,17 @@
 package shop.tryit.domain.answer;
 
+import java.util.List;
 import java.util.Optional;
+import shop.tryit.domain.question.Question;
 
 public interface AnswerRepository {
 
     Long save(Answer answer);
 
     Optional<Answer> findById(Long id);
+
+    List<Answer> findByQuestion(Question question);
+
+    void delete(Answer answer);
 
 }
