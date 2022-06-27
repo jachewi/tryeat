63c1f13e3e21f043828bb5871c6cb00aef23080a
AnneYuJeong lim <b.imnuj@gmail.com>
2022-06-28 00:24:38 +0900

    feat: 상품 등록 웹 계층 개발 (#48)
    * feat: 상품 등록 DTO 생성
    
    * feat: 상품 등록 폼 생성
    
    * feat: 상품 등록 컨트롤러 구현

---
+++ src/main/java/shop/tryit/web/item/ItemController.java
@@ -0,0 +1,57 @@
+package shop.tryit.web.item;
+
+import java.io.IOException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import shop.tryit.domain.item.Item;
+import shop.tryit.domain.item.ItemFile;
+import shop.tryit.domain.item.ItemFileStore;
+import shop.tryit.domain.item.ItemService;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService itemService;
+    private final ItemFileStore itemFileStore;
+
+    @GetMapping("/new")
+    public String newItemForm(Model model) {
+        model.addAttribute("item", ItemFormDto.builder().build());
+        return "/items/register";
+    }
+
+    @PostMapping("/new")
+    public String newItem(@ModelAttribute("item") ItemFormDto form) throws IOException {
+        log.info("item name = {}", form.getName());
+        log.info("item price = {}", form.getPrice());
+
+        ItemFile mainImage = itemFileStore.storeMainImage(form.getMainImage());
+        List<ItemFile> detailImages = itemFileStore.storeDetailImages(form.getDetailImage());
+
+        // DB에 저장
+        Item item = Item.builder()
+                .name(form.getName())
+                .category(form.getCategory())
+                .stockQuantity(form.getStockQuantity())
+                .price(form.getPrice())
+                .mainImage(mainImage)
+                .detailImage((detailImages))
+                .build();
+
+        itemService.register(item);
+
+        // TODO: items.html 생성
+        return "redirect:/";
+    }
+
+}
