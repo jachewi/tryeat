7590ece10ce74688ca68f31f808e54aee06cb669
kureungkureung <terry9611@naver.com>
2022-06-27 15:39:04 +0900

    feat: 답변 서비스에 트랜잭션 추가

--- src/main/java/shop/tryit/domain/answer/AnswerService.java
+++ src/main/java/shop/tryit/domain/answer/AnswerService.java
@@ -2,13 +2,16 @@ package shop.tryit.domain.answer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
 
+    @Transactional
     public Long register(Answer answer) {
         return answerRepository.save(answer);
     }
