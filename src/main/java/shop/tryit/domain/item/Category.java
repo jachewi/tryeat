f02fcf309003077d20666b71ad9da66f2443fffc
yujeong lim <b.imnuj@gmail.com>
2022-06-28 17:24:08 +0900

    refactor: 카테고리 엔티티에서 enum 타입으로 변경

--- src/main/java/shop/tryit/domain/item/Category.java
+++ src/main/java/shop/tryit/domain/item/Category.java
@@ -1,33 +1,7 @@
 package shop.tryit.domain.item;
 
-import static javax.persistence.GenerationType.IDENTITY;
-import static lombok.AccessLevel.PROTECTED;
+public enum Category {
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Getter
-@NoArgsConstructor(access = PROTECTED)
-public class Category {
-
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "category_id")
-    private Long id; // 카테고리 식별자
-
-    private String name; // 카테고리명 [CAT, DOG]
-
-    private Category(String name) {
-        this.name = name;
-    }
-
-    public static Category from(String name) {
-        return new Category(name);
-    }
+    CAT, DOG
 
 }
