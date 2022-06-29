3d83fc04665e56f1322c71343b96009c9a419e94
Kimjunghyunn <wjdgus6846@gmail.com>
2022-06-29 20:15:00 +0900

    refactor: 회원 조회 성공테스트 수정

--- src/test/java/shop/tryit/domain/member/MemberServiceTests.java
+++ src/test/java/shop/tryit/domain/member/MemberServiceTests.java
@@ -2,13 +2,13 @@ package shop.tryit.domain.member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import shop.tryit.repository.member.MemberJpaRepository;
+import javax.persistence.EntityManager;
 
 @Transactional
 @SpringBootTest
@@ -19,6 +19,9 @@ class MemberServiceTests {
 
     @Autowired
     private MemberJpaRepository memberJpaRepository;
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Test
     void 회원가입_테스트() {
@@ -69,11 +72,24 @@ class MemberServiceTests {
                 .password("11111")
                 .build();
 
+        memberJpaRepository.save(member1);
+
+        entityManager.flush();
+        entityManager.clear();
+
         //when
-        sut.saveMember(member1);
-        Member findEmail = sut.findMember("test1234@gmail.com");
+        Member findMember = sut.findMember("test1234@gmail.com");
 
         //then
-        assertNotNull(memberJpaRepository.findByEmail(String.valueOf(findEmail)));
+        assertThat(findMember).usingRecursiveComparison().ignoringFields("id").isEqualTo(member1);
     }
+
+    @Test
+    void 회원_조회_실패_테스트() {
+        //when
+        assertThatThrownBy(() -> sut.findMember("test1234@gmail.com"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("해당 회원을 찾을 수 없습니다.");
+    }
+
 }
