0622712b84048a549e39fe9d7fbaf4d8cc3c8fb1
JungHyun Kim <93319948+Kimjunghyunn@users.noreply.github.com>
2022-06-29 20:16:59 +0900

    refactor: 회원 이메일을 통해 회원 찾기 (#67)
    * refactor: 회원이메일을 통해 회원 찾기
    
    * refactor: 회원 조회 성공테스트 수정
    
    Co-authored-by: Kimjunghyunn <wjdgus6846@gmail.com>

--- src/test/java/shop/tryit/domain/member/MemberServiceTests.java
+++ src/test/java/shop/tryit/domain/member/MemberServiceTests.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import shop.tryit.repository.member.MemberJpaRepository;
+import javax.persistence.EntityManager;
 
 @Transactional
 @SpringBootTest
-class MemberServiceTest {
+class MemberServiceTests {
 
     @Autowired
     private MemberService sut;
@@ -19,21 +20,28 @@ class MemberServiceTest {
     @Autowired
     private MemberJpaRepository memberJpaRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Test
     void 회원가입_테스트() {
+        //given
         Member member = Member.builder()
                 .email("test1234@gmail.com")
                 .name("test")
                 .password("11111")
                 .build();
 
+        //when
         Long savedMember = sut.saveMember(member);
 
+        //then
         assertThat(memberJpaRepository.findById(savedMember).isPresent()).isTrue();
     }
 
     @Test
     void 중복_회원_성공_테스트() {
+        //given
         Member member1 = Member.builder()
                 .email("test1234@gmail.com")
                 .name("test")
@@ -46,11 +54,42 @@ class MemberServiceTest {
                 .password("11111")
                 .build();
 
+        //when
         sut.saveMember(member1);
 
+        //then
         assertThatThrownBy(() -> sut.saveMember(member2))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("이미 가입된 회원입니다.");
+    }
+
+    @Test
+    void 회원_조회_성공_테스트() {
+        //given
+        Member member1 = Member.builder()
+                .email("test1234@gmail.com")
+                .name("test")
+                .password("11111")
+                .build();
+
+        memberJpaRepository.save(member1);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        //when
+        Member findMember = sut.findMember("test1234@gmail.com");
+
+        //then
+        assertThat(findMember).usingRecursiveComparison().ignoringFields("id").isEqualTo(member1);
+    }
+
+    @Test
+    void 회원_조회_실패_테스트() {
+        //when
+        assertThatThrownBy(() -> sut.findMember("test1234@gmail.com"))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("해당 회원을 찾을 수 없습니다.");
     }
 
 }
