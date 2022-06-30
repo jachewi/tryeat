1aa18bda890f6101f5a7c3ec5f985109c30aa322
yujeong lim <b.imnuj@gmail.com>
2022-06-30 23:36:41 +0900

    feat: 상품 목록 엔티티를 상품 목록 DTO로 변환하는 기능 추가

--- src/main/java/shop/tryit/web/item/ItemAdapter.java
+++ src/main/java/shop/tryit/web/item/ItemAdapter.java
@@ -25,4 +25,21 @@ public class ItemAdapter {
         return item;
     }
 
+    public static List<ItemListDto> toListDto(List<Item> items, List<ItemFile> mainImages) {
+        List<ItemListDto> toListDtoResult = new ArrayList<>();
+
+        // 조회된 모든 Item -> ItemListDto 변환해서 리스트에 담음
+        for (int i = 0; i < items.size(); i++) {
+            ItemListDto listDto = ItemListDto.builder()
+                    .name(items.get(i).getName())
+                    .price(items.get(i).getPrice())
+                    .mainImage(mainImages.get(i))
+                    .build();
+
+            toListDtoResult.add(listDto);
+        }
+
+        return toListDtoResult;
+    }
+
 }
