5de11d0aeb13c7933d91bf4f6332125856411e4e
Kimjunghyunn <wjdgus6846@gmail.com>
2022-06-30 16:01:43 +0900

    feat: 회원 어댑터 수정용 toEntity 추가, toDto 수정

--- src/main/java/shop/tryit/web/member/MemberAdapter.java
+++ src/main/java/shop/tryit/web/member/MemberAdapter.java
@@ -18,12 +18,27 @@ public class MemberAdapter {
         return member;
     }
 
+    public static Member toEntity(MemberFormDto memberFormDto, Address address) {
+        Member member = Member.builder()
+                .email(memberFormDto.getEmail())
+                .name(memberFormDto.getName())
+                .password(memberFormDto.getPassword1())
+                .address(address)
+                .phoneNumber(memberFormDto.getPhone())
+                .build();
+
+        return member;
+    }
+
     public static MemberFormDto toDto(Member member) {
         MemberFormDto memberFormDto = MemberFormDto.builder()
                 .email(member.getEmail())
                 .name(member.getName())
-                .password1(member.getPassword())
                 .phone(member.getPhoneNumber())
+                .zipcode(member.getAddress().getZipcode())
+                .jibeon_address(member.getAddress().getJibeon_address())
+                .street_address(member.getAddress().getStreet_address())
+                .detail_address(member.getAddress().getDetail_address())
                 .build();
 
         return memberFormDto;
