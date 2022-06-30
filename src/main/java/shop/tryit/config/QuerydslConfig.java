8fa9292312e8f841edab552ee11a8b36c25649eb
kureung <89060425+kureung@users.noreply.github.com>
2022-06-30 15:55:53 +0900

    feat: querydsl 설정 추가 (#76)
    * feat: jpa osiv 설정 false로 세팅
    
    * feat: querydsl build 설정
    
    * feat: JPAQueryFactory 빈 등록

---
+++ src/main/java/shop/tryit/config/QuerydslConfig.java
@@ -0,0 +1,20 @@
+package shop.tryit.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Bean
+    public JPAQueryFactory queryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+
+}
