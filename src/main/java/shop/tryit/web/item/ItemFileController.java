f1eb5a08202ac62199bb14322bdd3ff1de94b1e9
yujeong lim <b.imnuj@gmail.com>
2022-06-30 23:48:45 +0900

    feat: 상품 이미지 파일 컨트롤러 생성
    - 업로드 된 이미지 파일을 로컬에서 가져올 때 보안 때문에 실제로는 엑박이 뜨는 문제 발생
    - 해당 문제를 해결하기 위해 컨트롤러를 활용

---
+++ src/main/java/shop/tryit/web/item/ItemFileController.java
@@ -0,0 +1,28 @@
+package shop.tryit.web.item;
+
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import shop.tryit.domain.item.ItemFileStore;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemFileController {
+
+    private final ItemFileStore itemFileStore;
+
+    @ResponseBody
+    @GetMapping("/files/{filename}")
+    public Resource viewMainImage(@PathVariable String filename) throws IOException {
+        log.info("mainImage.getStoreFileName = {}", filename);
+        return new UrlResource("file:" + itemFileStore.getFullPath(filename));
+    }
+
+}
