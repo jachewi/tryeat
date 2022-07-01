17222025d210f5deea1b66ddab91a34f9e02a740
Kimjunghyunn <wjdgus6846@gmail.com>
2022-07-01 15:57:25 +0900

    feat: OAuth2 카카오 로그인 서비스 로직 추가

--- src/main/java/shop/tryit/domain/member/MemberService.java
+++ src/main/java/shop/tryit/domain/member/MemberService.java
@@ -1,6 +1,10 @@
 package shop.tryit.domain.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +16,8 @@ public class MemberService {
 
     private final MemberRepository repository;
     private final PasswordEncoder passwordEncoder;
+    private final KakaoOAuth2 kakaoOAuth2;
+    private final AuthenticationManager authenticationManager;
 
     @Transactional
     public Long saveMember(Member member) {
@@ -40,4 +46,58 @@ public class MemberService {
         return findMember.getEmail();
     }
 
+    public void kakaoLogin(String authorizedCode) {
+        // 카카오 OAuth2 를 통해 카카오 사용자 정보 조회
+        KakaoMember userInfo = kakaoOAuth2.getUserInfo(authorizedCode);
+        Long kakaoId = userInfo.getId();
+        String nickname = userInfo.getNickname();
+        String email = userInfo.getEmail();
+
+        // 우리 DB 에서 회원 Id 와 패스워드
+        // 회원 Id = 카카오 nickname
+        String username = nickname;
+        // 패스워드 = 카카오 Id + ADMIN TOKEN
+        String password = kakaoId + "admintoken";
+
+        String kakaoEmail = email;
+
+        // DB 에 중복된 email 이 있는지 확인
+        Member kakaoUser = repository.findByEmail(email).orElse(null);
+
+        // 카카오 정보로 회원가입
+        if (kakaoUser == null) {
+            // 패스워드 인코딩
+            String encodedPassword = passwordEncoder.encode(password);
+            // ROLE = 사용자
+            MemberRole role = MemberRole.USER;
+
+            System.out.println("카카오 아이디: " + kakaoId);
+            System.out.println("카카오 이메일: " + email);
+            System.out.println("서버 아이디: " + kakaoEmail);
+            System.out.println("서버 이름: " + username);
+            System.out.println("서버 패스워드: " + encodedPassword);
+
+            kakaoUser = Member.builder()
+                    .email(kakaoEmail)
+                    .password(encodedPassword)
+                    .name(username)
+                    .role(role)
+                    .build();
+
+            repository.save(kakaoUser);
+        }else {
+            System.out.println("기존 회원이므로 로그인을 시도");
+
+            Member existingMember = findMember(email);
+            Authentication kakaoUsernamePassword = new UsernamePasswordAuthenticationToken(existingMember.getEmail(), existingMember.getPassword());
+            Authentication authentication = authenticationManager.authenticate(kakaoUsernamePassword);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        System.out.println("세이브 후 로그인 처리 시작");
+        // 로그인 처리
+        Authentication kakaoUsernamePassword = new UsernamePasswordAuthenticationToken(kakaoUser.getName(), kakaoUser.getPassword());
+        Authentication authentication = authenticationManager.authenticate(kakaoUsernamePassword);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
 }
