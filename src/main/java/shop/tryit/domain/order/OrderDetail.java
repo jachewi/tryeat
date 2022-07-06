1ad51ccdffdb944771c9e4bc2ca16d55ef64ec9e
Kimjunghyunn <wjdgus6846@gmail.com>
2022-07-06 17:49:49 +0900

    feat: 주문 상세 엔티티 생성

---
+++ src/main/java/shop/tryit/domain/order/OrderDetail.java
@@ -0,0 +1,48 @@
+package shop.tryit.domain.order;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.tryit.domain.item.Item;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "order_detail_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; // 주문 가격
+
+    private int count; //주문 수량
+
+    @Builder
+    private OrderDetail(Item item, Order order, int orderPrice, int count) {
+        this.item = item;
+        this.order = order;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+
+}
