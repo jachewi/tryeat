5ddc5d2a08152857b7253d9f45a6202c27f39143
kureungkureung <terry9611@naver.com>
2022-07-09 01:31:37 +0900

    feat: 답변 서비스에 특정 질문의 답변 페이징 기능 구현

--- src/main/java/shop/tryit/domain/answer/AnswerService.java
+++ src/main/java/shop/tryit/domain/answer/AnswerService.java
@@ -1,14 +1,21 @@
 package shop.tryit.domain.answer;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.tryit.domain.question.Question;
+import shop.tryit.domain.question.QuestionService;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AnswerService {
 
+    private final QuestionService questionService;
     private final AnswerRepository answerRepository;
 
     @Transactional
@@ -31,6 +38,13 @@ public class AnswerService {
     private Answer findOne(Long id) {
         return answerRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 답변을 찾을 수 없습니다."));
+    }
+
+    public Page<Answer> findAnswersByQuestionId(Long questionId, int pageNumber) {
+        Question question = questionService.findOne(questionId);
+        Sort sortBy = Sort.by(Direction.DESC, "createDate");
+        PageRequest pageRequest = PageRequest.of(pageNumber, 3, sortBy);
+        return answerRepository.findSearchByQuestion(question, pageRequest);
     }
 
 }
