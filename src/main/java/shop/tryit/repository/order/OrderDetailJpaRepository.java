78862f2d20e5f96901f2b64b392ad8f25681559f
Kimjunghyunn <wjdgus6846@gmail.com>
2022-07-09 15:57:17 +0900

    refactor: 주문 관련 JPA 레포지토리 수정

--- src/main/java/shop/tryit/repository/order/OrderDetailJpaRepository.java
+++ src/main/java/shop/tryit/repository/order/OrderDetailJpaRepository.java
@@ -1,10 +1,8 @@
 package shop.tryit.repository.order;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.tryit.domain.order.OrderDetail;
 
 public interface OrderDetailJpaRepository extends JpaRepository<OrderDetail, Long> {
-    Optional<OrderDetail> findById(Long id);
 
 }
