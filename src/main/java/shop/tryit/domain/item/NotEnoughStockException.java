5d66a25f8bdec7a01dea8a9dff900697168514a1
Kimjunghyunn <wjdgus6846@gmail.com>
2022-07-12 16:32:28 +0900

    feat: 상품 수량 관련 에러클래스 생성

---
+++ src/main/java/shop/tryit/domain/item/NotEnoughStockException.java
@@ -0,0 +1,19 @@
+package shop.tryit.domain.item;
+
+public class NotEnoughStockException extends RuntimeException {
+    public NotEnoughStockException() {
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+}
