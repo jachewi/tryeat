5b3ac93c8a293ab4dfef158230b7b7d5eef0993e
kureungkureung <terry9611@naver.com>
2022-07-15 03:40:07 +0900

    feat: 공지 서비스 생성

---
+++ src/main/java/shop/tryit/domain/notice/NoticeService.java
@@ -0,0 +1,12 @@
+package shop.tryit.domain.notice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+}
