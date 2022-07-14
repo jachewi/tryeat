12ff4101557396eb621159f98b0bb63a3f4055af
kureungkureung <terry9611@naver.com>
2022-07-15 03:25:32 +0900

    feat: 공지 리포지토리 구현체에 JPA 리포지토리 의존성 추가

--- src/main/java/shop/tryit/repository/notice/NoticeRepositoryImpl.java
+++ src/main/java/shop/tryit/repository/notice/NoticeRepositoryImpl.java
@@ -1,7 +1,12 @@
 package shop.tryit.repository.notice;
 
+import lombok.RequiredArgsConstructor;
 import shop.tryit.domain.notice.NoticeRepository;
 
+
+@RequiredArgsConstructor
 public class NoticeRepositoryImpl implements NoticeRepository {
+
+    private final NoticeJpaRepository jpaRepository;
 
 }
