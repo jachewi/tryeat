3d118f00c9ba1881cd2daad3f6e62e27707038cd
kureungkureung <terry9611@naver.com>
2022-07-15 05:09:30 +0900

    feat: 공지 단건 조회 폼 구현

---
+++ src/main/java/shop/tryit/web/notice/NoticeViewFormDto.java
@@ -0,0 +1,24 @@
+package shop.tryit.web.notice;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class NoticeViewFormDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String memberEmail;
+
+    @Builder
+    private NoticeViewFormDto(Long id, String title, String content, String memberEmail) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.memberEmail = memberEmail;
+    }
+
+}
