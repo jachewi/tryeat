501675726580801c128752b8cf10dc67d3bd69d8
kureungkureung <terry9611@naver.com>
2022-07-15 06:16:05 +0900

    feat: 공지 업데이트 폼 구현

---
+++ src/main/java/shop/tryit/web/notice/NoticeUpdateFormDto.java
@@ -0,0 +1,30 @@
+package shop.tryit.web.notice;
+
+import javax.validation.constraints.NotEmpty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class NoticeUpdateFormDto {
+
+    private Long id;
+
+    @NotEmpty(message = "제목을 입력해주세요.")
+    private String title;
+
+    @NotEmpty(message = "내용을 입력해주세요.")
+    private String content;
+
+    private String memberEmail;
+
+    @Builder
+    private NoticeUpdateFormDto(Long id, String title, String content, String memberEmail) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.memberEmail = memberEmail;
+    }
+
+}
