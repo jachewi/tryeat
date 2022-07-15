14ff49580f1e47edf792607fdb5765784f9d6847
kureung <89060425+kureung@users.noreply.github.com>
2022-07-15 12:17:13 +0900

    feat: 공지 서비스의 CRUD 기능 구현 (#141)
    * feat: 공지 서비스 생성
    
    * feat: 공지 서비스에 저장 기능 구현
    
    * feat: 공지 서비스에 단건 조회 기능 구현
    
    * feat: 공지 서비스에 트랜잭션 추가
    
    * feat: 공지 서비스의 단건 조회 테스트
    
    * refactor: 공지 서비스에 저장 기능에 트랜잭션 추가
    
    * feat: 공지 서비스에 업데이트 기능 구현
    
    * feat: 공지 서비스에 삭제 기능 구현

---
+++ src/main/java/shop/tryit/domain/notice/NoticeService.java
@@ -0,0 +1,35 @@
+package shop.tryit.domain.notice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    @Transactional
+    public Long save(Notice notice) {
+        return noticeRepository.save(notice);
+    }
+
+    public Notice findById(Long id) {
+        return noticeRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("해당하는 공지사항이 없습니다."));
+    }
+
+    @Transactional
+    public void update(Long id, Notice newNotice) {
+        Notice notice = findById(id);
+        notice.update(newNotice.getTitle(), newNotice.getContent());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        noticeRepository.delete(findById(id));
+    }
+
+}
