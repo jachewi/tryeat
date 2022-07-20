48574cdb04a13442450e8ccec597a09ff5713b37
Kimjunghyunn <wjdgus6846@gmail.com>
2022-07-20 18:40:08 +0900

    feat: 결제 레포지토리 구현체 생성

---
+++ src/main/java/shop/tryit/repository/payment/PaymentRepositoryImpl.java
@@ -0,0 +1,21 @@
+package shop.tryit.repository.payment;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import shop.tryit.domain.payment.Payment;
+import shop.tryit.domain.payment.PaymentRepository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class PaymentRepositoryImpl implements PaymentRepository {
+
+    private final PaymentJpaRepository jpaRepository;
+
+    @Override
+    public Long save(Payment payment) {
+        return jpaRepository.save(payment).getId();
+    }
+
+}
