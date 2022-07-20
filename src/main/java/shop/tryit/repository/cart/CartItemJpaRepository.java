e28ba041fc5c1f754047e489827158187610c1ef
yujeong <b.imnuj@gmail.com>
2022-07-20 19:16:44 +0900

    feat: 장바구니 상품 레포지토리에 장바구니와 상품으로 조회하는 기능 추가

--- src/main/java/shop/tryit/repository/cart/CartItemJpaRepository.java
+++ src/main/java/shop/tryit/repository/cart/CartItemJpaRepository.java
@@ -1,8 +1,12 @@
 package shop.tryit.repository.cart;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import shop.tryit.domain.cart.entity.Cart;
 import shop.tryit.domain.cart.entity.CartItem;
+import shop.tryit.domain.item.Item;
 
 public interface CartItemJpaRepository extends JpaRepository<CartItem, Long> {
+
+    CartItem findByCartAndItem(Cart cart, Item item);
 
 }
--- src/main/java/shop/tryit/repository/cart/CartItemRepositoryImpl.java
+++ src/main/java/shop/tryit/repository/cart/CartItemRepositoryImpl.java
@@ -2,8 +2,10 @@ package shop.tryit.repository.cart;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import shop.tryit.domain.cart.entity.Cart;
 import shop.tryit.domain.cart.entity.CartItem;
 import shop.tryit.domain.cart.repository.CartItemRepository;
+import shop.tryit.domain.item.Item;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,6 +16,11 @@ public class CartItemRepositoryImpl implements CartItemRepository {
     @Override
     public Long save(CartItem cartItem) {
         return cartItemJpaRepository.save(cartItem).getId();
+    }
+
+    @Override
+    public CartItem findByCartAndItem(Cart cart, Item item) {
+        return cartItemJpaRepository.findByCartAndItem(cart, item);
     }
 
 }
