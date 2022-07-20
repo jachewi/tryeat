dda964666b5b05e603acb8927367224b175a9f38
yujeong <b.imnuj@gmail.com>
2022-07-20 19:11:19 +0900

    refactor: 장바구니 생성 비즈니스 로직 수정

--- src/main/java/shop/tryit/domain/cart/service/CartService.java
+++ src/main/java/shop/tryit/domain/cart/service/CartService.java
@@ -1,10 +1,14 @@
 package shop.tryit.domain.cart.service;
 
+import static java.util.Objects.isNull;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.tryit.domain.cart.entity.Cart;
 import shop.tryit.domain.cart.repository.CartRepository;
+import shop.tryit.domain.member.Member;
+import shop.tryit.domain.member.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -12,12 +16,27 @@ import shop.tryit.domain.cart.repository.CartRepository;
 public class CartService {
 
     private final CartRepository cartRepository;
+    private final MemberRepository memberRepository;
 
     /**
-     * 장바구니 등록
+     * 장바구니 생성
      */
-    public Long register(Cart cart) {
-        return cartRepository.save(cart);
+    @Transactional
+    public Long createCart(String email) {
+        // 현재 로그인한 회원 엔티티 조회
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalStateException("해당 회원을 찾을 수 없습니다."));
+
+        // 회원의 장바구니 엔티티 조회
+        Cart cart = cartRepository.findByMember(member);
+
+        // 회원에게 장바구니가 없을 경우, 장바구니 생성
+        if (isNull(cart)) {
+            cart = Cart.from(member);
+            cartRepository.save(cart);
+        }
+
+        return cart.getId();
     }
 
 }
