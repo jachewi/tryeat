af2083a3ce94768a7c81cb6031098efd653cb8f5
AnneYuJeong lim <b.imnuj@gmail.com>
2022-07-21 14:19:57 +0900

    feat: 장바구니 상품 추가 서비스 로직 개발 (#175)
    * feat: 장바구니 상품 레포지토리에 장바구니와 상품으로 조회하는 기능 추가
    
    * feat: 장바구니 상품 엔티티에 개수를 증가시키는 비즈니스 로직 추가
    - 장바구니에 이미 상품이 있을 경우 장바구니 상품의 개수를 증가시키기 위함
    
    * refactor: 장바구니 상품 추가 비즈니스 로직 수정
    
    * refactor: 장바구니 상품 추가 테스트 수정
    - 장바구니에 상품이 이미 담겨있는 경우와 그렇지 않을 경우를 나눠서 테스트 코드를 구현

--- src/main/java/shop/tryit/domain/cart/repository/CartItemRepository.java
+++ src/main/java/shop/tryit/domain/cart/repository/CartItemRepository.java
@@ -1,9 +1,13 @@
 package shop.tryit.domain.cart.repository;
 
+import shop.tryit.domain.cart.entity.Cart;
 import shop.tryit.domain.cart.entity.CartItem;
+import shop.tryit.domain.item.Item;
 
 public interface CartItemRepository {
 
     Long save(CartItem cartItem);
+
+    CartItem findByCartAndItem(Cart cart, Item item);
 
 }
--- src/test/java/shop/tryit/domain/cart/CartItemServiceTests.java
+++ src/test/java/shop/tryit/domain/cart/CartItemServiceTests.java
@@ -1,6 +1,7 @@
 package shop.tryit.domain.cart;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,28 +9,58 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import shop.tryit.domain.cart.entity.CartItem;
 import shop.tryit.domain.cart.service.CartItemService;
+import shop.tryit.domain.item.Item;
+import shop.tryit.domain.item.ItemRepository;
 import shop.tryit.repository.cart.CartItemJpaRepository;
 
 @Transactional
 @SpringBootTest
-class CartItemServiceTests {
-
+public class CartItemServiceTests {
     @Autowired
     CartItemJpaRepository cartItemJpaRepository;
 
     @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
     CartItemService sut;
 
+    private Item saveItem() {
+        Item item = Item.builder().build();
+        itemRepository.save(item);
+        return item;
+    }
+
     @Test
-    void 장바구니_상품_등록() {
+    void 장바구니에_상품이_없다면_상품_추가() {
         // given
-        CartItem cartItem = CartItem.builder().build();
+        Item item = saveItem();
+
+        CartItem cartItem = CartItem.builder().item(item).build();
 
         // when
-        Long savedId = sut.register(cartItem);
+        Long savedCartItemId = sut.addCartItem(cartItem); // 장바구니 상품 아이디
 
         // then
-        assertTrue(cartItemJpaRepository.findById(savedId).isPresent());
+        assertNotNull(cartItemJpaRepository.findById(savedCartItemId));
+    }
+
+    @Test
+    void 장바구니에_상품이_있다면_개수_증가() {
+        // given
+        Item item = saveItem();
+
+        CartItem cartItem = CartItem.builder()
+                .item(item)
+                .count(2)
+                .build();
+        cartItemJpaRepository.save(cartItem);
+
+        // when
+        Long savedCartItemId = sut.addCartItem(cartItem);
+
+        // then
+        assertThat(cartItemJpaRepository.findById(savedCartItemId).get().getCount()).isEqualTo(4);
     }
 
 }
