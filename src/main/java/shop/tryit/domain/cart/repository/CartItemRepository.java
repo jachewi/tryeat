de5e7a2ade4bf3a038baf8d881bc87702f5adc3b
AnneYuJeong lim <b.imnuj@gmail.com>
2022-07-21 17:55:11 +0900

    feat: 장바구니 조회 서비스 로직 개발 (#179)
    * feat: 장바구니 상품 레포지토리에 장바구니로 조회하는 기능 추가
    
    * feat: 장바구니에 담긴 상품을 조회하는 비즈니스 로직 추가
    
    * feat: 장바구니에 담긴 상품을 조회하는 비즈니스 로직 테스트

--- src/main/java/shop/tryit/domain/cart/repository/CartItemRepository.java
+++ src/main/java/shop/tryit/domain/cart/repository/CartItemRepository.java
@@ -1,5 +1,6 @@
 package shop.tryit.domain.cart.repository;
 
+import java.util.List;
 import shop.tryit.domain.cart.entity.Cart;
 import shop.tryit.domain.cart.entity.CartItem;
 import shop.tryit.domain.item.Item;
@@ -9,5 +10,7 @@ public interface CartItemRepository {
     Long save(CartItem cartItem);
 
     CartItem findByCartAndItem(Cart cart, Item item);
+
+    List<CartItem> findByCart(Cart cart);
 
 }
--- src/main/java/shop/tryit/repository/cart/CartItemJpaRepository.java
+++ src/main/java/shop/tryit/repository/cart/CartItemJpaRepository.java
@@ -1,5 +1,6 @@
 package shop.tryit.repository.cart;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.tryit.domain.cart.entity.Cart;
 import shop.tryit.domain.cart.entity.CartItem;
@@ -8,5 +9,7 @@ import shop.tryit.domain.item.Item;
 public interface CartItemJpaRepository extends JpaRepository<CartItem, Long> {
 
     CartItem findByCartAndItem(Cart cart, Item item);
+
+    List<CartItem> findByCart(Cart cart);
 
 }
