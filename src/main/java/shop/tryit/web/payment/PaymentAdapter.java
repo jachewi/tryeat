564e462522e6a78b2fd0742d2789714657219a3a
Kimjunghyunn <wjdgus6846@gmail.com>
2022-07-22 23:15:53 +0900

    refactor: 결제 어댑터에 toDto 메소드 추가

--- src/main/java/shop/tryit/web/payment/PaymentAdapter.java
+++ src/main/java/shop/tryit/web/payment/PaymentAdapter.java
@@ -1,5 +1,8 @@
 package shop.tryit.web.payment;
 
+import shop.tryit.domain.item.Image;
+import shop.tryit.domain.item.Item;
+import shop.tryit.domain.member.Member;
 import shop.tryit.domain.payment.Payment;
 
 public class PaymentAdapter {
@@ -14,6 +17,22 @@ public class PaymentAdapter {
         return PaymentDto.builder()
                 .number(payment.getNumber())
                 .totalPrice(payment.getTotalPrice())
+                .build();
+    }
+
+    public static PaymentResponseDto toDto(Item item, Image mainImage, Member member, PaymentRequsetDto paymentRequsetDto) {
+        return PaymentResponseDto.builder()
+                .itemId(item.getId())
+                .mainImage(mainImage)
+                .itemName(item.getName())
+                .quantity(paymentRequsetDto.getQuantity())
+                .totalPrice(item.getPrice() * paymentRequsetDto.getQuantity())
+                .memberName(member.getName())
+                .memberPhone(member.getPhoneNumber())
+                .zipcode(member.addressZipcode())
+                .street_address(member.addressStreet())
+                .jibeon_address(member.addressJibeon())
+                .detail_address(member.addressDetail())
                 .build();
     }
 
