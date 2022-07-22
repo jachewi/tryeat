41cec975e2a27fa9776e1f944917b656efcd62ba
kureung <89060425+kureung@users.noreply.github.com>
2022-07-22 21:15:39 +0900

    feat: 주문 레포지토리에서 주문 페이징 기능 구현 (#196)
    * feat: 주문 페이징 기능 구현
    
    * feat: data jpa에 querydsl 리포지토리 의존성 추가
    
    * refactor: 주문 상세 생성자 변경

---
+++ src/main/java/shop/tryit/repository/order/OrderCustomImpl.java
@@ -0,0 +1,65 @@
+package shop.tryit.repository.order;
+
+import static shop.tryit.domain.order.QOrder.order;
+
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import shop.tryit.domain.member.Member;
+import shop.tryit.domain.order.Order;
+import shop.tryit.domain.order.OrderDetailRepository;
+import shop.tryit.domain.order.OrderDetailSearchDto;
+import shop.tryit.domain.order.OrderSearchDto;
+import shop.tryit.domain.order.QOrderSearchDto;
+
+@RequiredArgsConstructor
+public class OrderCustomImpl implements OrderCustom {
+
+    private final JPAQueryFactory queryFactory;
+    private final OrderDetailRepository orderDetailRepository;
+
+    @Override
+    public Page<OrderSearchDto> searchOrders(Member member, Pageable pageable) {
+        return PageableExecutionUtils.getPage(orderSearchContents(member), pageable, totalCount(member)::fetchOne);
+    }
+
+    private List<OrderSearchDto> orderSearchContents(Member member) {
+        QOrderSearchDto qOrderSearchDto = new QOrderSearchDto(order.id, order.number, order.createDate, order.status);
+
+        List<OrderSearchDto> content = queryFactory
+                .select(qOrderSearchDto)
+                .from(order)
+                .where(order.member.eq(member))
+                .fetch();
+
+        content.forEach(this::injectOrderDetails);
+        return content;
+    }
+
+    private void injectOrderDetails(OrderSearchDto orderSearchDto) {
+        Long orderId = orderSearchDto.getOrderId();
+        Order order = findById(orderId);
+
+        List<OrderDetailSearchDto> orderDetailSearchDtos = orderDetailRepository.searchOrderDetails(order);
+        orderSearchDto.injectOrderDetails(orderDetailSearchDtos);
+    }
+
+    public JPAQuery<Long> totalCount(Member member) {
+        return queryFactory.select(order.count())
+                .from(order)
+                .where(order.member.eq(member));
+    }
+
+    private Order findById(Long orderId) {
+        return queryFactory
+                .select(order)
+                .from(order)
+                .where(order.id.eq(orderId))
+                .fetchOne();
+    }
+
+}
--- src/main/java/shop/tryit/repository/order/OrderJpaRepository.java
+++ src/main/java/shop/tryit/repository/order/OrderJpaRepository.java
@@ -3,6 +3,6 @@ package shop.tryit.repository.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.tryit.domain.order.Order;
 
-public interface OrderJpaRepository extends JpaRepository<Order, Long> {
+public interface OrderJpaRepository extends JpaRepository<Order, Long>, OrderCustom {
 
 }
