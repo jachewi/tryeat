dd119b74878839c6b7a0dd4a03419ca56bca205c
kureungkureung <terry9611@naver.com>
2022-07-22 20:30:46 +0900

    feat: data jpa에 querydsl 레포지토리 의존성 추가

--- src/main/java/shop/tryit/repository/order/OrderDetailJpaRepository.java
+++ src/main/java/shop/tryit/repository/order/OrderDetailJpaRepository.java
@@ -3,6 +3,6 @@ package shop.tryit.repository.order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.tryit.domain.order.OrderDetail;
 
-public interface OrderDetailJpaRepository extends JpaRepository<OrderDetail, Long> {
+public interface OrderDetailJpaRepository extends JpaRepository<OrderDetail, Long>, OrderDetailCustom {
 
 }
