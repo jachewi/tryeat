c40bb50d46726d29cba48c54faa5bb7a98c7dea6
kureung <89060425+kureung@users.noreply.github.com>
2022-07-25 17:05:14 +0900

    refactor: 주문 dto에 주소지 추가 (#202)

--- src/main/java/shop/tryit/domain/order/OrderSearchDto.java
+++ src/main/java/shop/tryit/domain/order/OrderSearchDto.java
@@ -12,18 +12,29 @@ public class OrderSearchDto {
     private String orderNumber;
     private LocalDateTime createDate;
     private OrderStatus orderStatus;
-
     private List<OrderDetailSearchDto> orderDetails;
+    private Long zipcode;
+    private String streetAddress;
+    private String jibeonAddress;
+    private String detailAddress;
 
     @QueryProjection
     public OrderSearchDto(Long orderId,
                           String orderNumber,
                           LocalDateTime createDate,
-                          OrderStatus orderStatus) {
+                          OrderStatus orderStatus,
+                          Long zipcode,
+                          String streetAddress,
+                          String jibeonAddress,
+                          String detailAddress) {
         this.orderId = orderId;
         this.orderNumber = orderNumber;
         this.createDate = createDate;
         this.orderStatus = orderStatus;
+        this.zipcode = zipcode;
+        this.streetAddress = streetAddress;
+        this.jibeonAddress = jibeonAddress;
+        this.detailAddress = detailAddress;
     }
 
     public void injectOrderDetails(List<OrderDetailSearchDto> orderDetails) {
