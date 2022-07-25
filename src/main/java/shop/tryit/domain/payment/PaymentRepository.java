073ff7e6fa2a3473f0d0a278323184537b764b7c
Kimjunghyunn <wjdgus6846@gmail.com>
2022-07-25 17:07:58 +0900

    refactor: findByNum 추가

--- src/main/java/shop/tryit/domain/payment/PaymentRepository.java
+++ src/main/java/shop/tryit/domain/payment/PaymentRepository.java
@@ -1,7 +1,11 @@
 package shop.tryit.domain.payment;
 
+import java.util.Optional;
+
 public interface PaymentRepository {
 
     Long save(Payment payment);
+
+    Optional<Payment> findByNum(Long Number);
 
 }
