9433a67753b297ac4727dfedb6e5e61bd68e00a2
kureungkureung <terry9611@naver.com>
2022-07-27 21:52:45 +0900

    feat : 주문 퍼사드 패턴 구현

---
+++ src/main/java/shop/tryit/domain/order/OrderFacade.java
@@ -0,0 +1,55 @@
+package shop.tryit.domain.order;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.userdetails.User;
+import shop.tryit.domain.item.Item;
+import shop.tryit.domain.item.ItemService;
+import shop.tryit.domain.member.MemberService;
+import shop.tryit.web.order.OrderDto;
+import shop.tryit.web.order.OrderFormDto;
+
+@RequiredArgsConstructor
+public class OrderFacade {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    public Page<OrderSearchDto> searchOrders(int page, User user) {
+        String email = user.getUsername();
+        return orderService.searchOrders(page, email);
+    }
+
+    public OrderDetail toEntity(OrderFormDto orderFormDto, Item item, Order order) {
+        return OrderDetail.of(item, order, orderFormDto.getOrderQuantity());
+    }
+
+    public OrderFormDto toDto(OrderDetail orderDetail) {
+        return OrderFormDto.builder()
+                .itemId(orderDetail.getItem().getId())
+                .itemName(orderDetail.getItem().getName())
+                .orderQuantity(orderDetail.getQuantity())
+                .build();
+    }
+
+    public List<OrderDto> toListDto(List<OrderDetail> orderDetails) {
+        return orderDetails.stream()
+                .map(this::toOrderDto)
+                .collect(toList());
+    }
+
+    private OrderDto toOrderDto(OrderDetail orderDetail) {
+        return OrderDto.builder()
+                .detailId(orderDetail.getId())
+                .number(orderDetail.orderNumber())
+                .itemName(orderDetail.itemName())
+                .date(orderDetail.orderCreateDate())
+                .status(orderDetail.orderStatus())
+                .build();
+    }
+
+}
