c29f6d6c3db215bc6a3895b44e9d0b02d612be86
kureungkureung <terry9611@naver.com>
2022-07-28 14:08:46 +0900

    refactor: 퍼사드에 트랜잭션 추가

--- src/main/java/shop/tryit/domain/order/service/OrderFacade.java
+++ src/main/java/shop/tryit/domain/order/service/OrderFacade.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import shop.tryit.domain.item.entity.Item;
 import shop.tryit.domain.order.Order;
 import shop.tryit.domain.order.OrderDetail;
@@ -16,6 +17,7 @@ import shop.tryit.domain.order.dto.OrderSearchDto;
 
 @Component
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OrderFacade {
 
     private final OrderService orderService;
