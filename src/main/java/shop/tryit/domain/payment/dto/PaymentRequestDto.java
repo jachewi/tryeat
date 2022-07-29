6413c460b803d104999f82642a5f54f62a8a1578
yujeong <b.imnuj@gmail.com>
2022-07-29 23:58:27 +0900

    style: 결제 요청 dto 이름 오타 수정

--- src/main/java/shop/tryit/domain/payment/dto/PaymentRequestDto.java
+++ src/main/java/shop/tryit/domain/payment/dto/PaymentRequestDto.java
@@ -3,9 +3,10 @@ package shop.tryit.domain.payment.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/* 결제 할 상품의 데이터를 받음 */
 @Data
 @NoArgsConstructor
-public class PaymentRequsetDto {
+public class PaymentRequestDto {
 
     private Long itemId;
 
