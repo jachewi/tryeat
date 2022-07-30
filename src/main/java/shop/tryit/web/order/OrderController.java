c12a3930fd8f0f73e2fe908540c57d6d2c2e2880
Kimjunghyunn <wjdgus6846@gmail.com>
2022-07-30 14:40:59 +0900

    refactor: OrderController로 데이터가 잘 넘어가는지 확인
    - 넘어온 데이터를 이용하여 주문 저장 서비스 로직을 작성하면 될 것 같아요!

--- src/main/java/shop/tryit/web/order/OrderController.java
+++ src/main/java/shop/tryit/web/order/OrderController.java
@@ -1,22 +1,23 @@
 package shop.tryit.web.order;
 
-import javax.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import shop.tryit.domain.common.Pages;
-import shop.tryit.domain.order.dto.OrderFormDto;
+import shop.tryit.domain.order.dto.OrderDetailDto;
 import shop.tryit.domain.order.dto.OrderSearchDto;
 import shop.tryit.domain.order.service.OrderFacade;
 
@@ -29,11 +30,14 @@ public class OrderController {
     private final OrderFacade orderFacade;
 
     @PostMapping("/new")
-    public String newOrder(@Valid @ModelAttribute OrderFormDto orderFormDto,
-                           BindingResult bindingResult,
-                           @AuthenticationPrincipal User user) {
+    public ResponseEntity<String> newOrder(@RequestBody List<OrderDetailDto> orderFormDto,
+                                           BindingResult bindingResult,
+                                           @AuthenticationPrincipal User user) {
+        log.info("======== 결제 폼 컨트롤러 시작 ========");
+        log.info("orderFormDto: {}", orderFormDto.toString());
+
         // TODO : 주문 등록 기능 구현
-        return "redirect:/orders";
+        return ResponseEntity.ok(orderFormDto.toString());
     }
 
     @GetMapping
