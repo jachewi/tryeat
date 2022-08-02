675cc697cb0c2d269a7f6510df92d232414ec729
JungHyun Kim <93319948+Kimjunghyunn@users.noreply.github.com>
2022-08-02 22:08:09 +0900

    feat: 결제 컨트롤러 검증 추가 (#289)
    * feat: 주문 수량이 상품재고 보다 많을 경우 검증 처리 추가
    
    * feat: 장바구니에서 넘어온 상품 수량 체크 메서드 추가
    
    * refactor: newPaymentForm 메서드 BindingResult 위치 변경
    
    Co-authored-by: Kimjunghyunn <wjdgus6846@gmail.com>

--- src/main/java/shop/tryit/web/payment/PaymentController.java
+++ src/main/java/shop/tryit/web/payment/PaymentController.java
@@ -1,6 +1,7 @@
 package shop.tryit.web.payment;
 
 import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,13 +30,22 @@ public class PaymentController {
     private final PaymentFacade paymentFacade;
 
     @GetMapping
-    public String newPaymentForm(@ModelAttribute PaymentRequestListDto paymentRequestListDto,
-                                 @AuthenticationPrincipal User user,
-                                 Model model) {
+    public String newPaymentForm(@Valid @ModelAttribute PaymentRequestListDto paymentRequestListDto,
+                                 BindingResult bindingResult, @AuthenticationPrincipal User user, Model model) {
         log.info("======== 결제 폼 컨트롤러 시작 ========");
 
         List<PaymentRequestDto> payments = paymentRequestListDto.getPayments();
-        log.info(payments.toString());
+
+        // 장바구니에서 상품 재고보다 많은 수량을 주문 했을 경우
+        if (Boolean.FALSE.equals(paymentFacade.checkItemStock(payments))) {
+            bindingResult.rejectValue("payments", "StockError",
+                    "상품 재고보다 많은 상품을 주문하였습니다.%n" + "장바구니를 다시 확인해주세요.");
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult = {}", bindingResult);
+            return "redirect:/carts";
+        }
 
         model.addAttribute("payments", paymentFacade.paymentForm(payments));
         model.addAttribute("member", paymentFacade.paymentForm(user));
