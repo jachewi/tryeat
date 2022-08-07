04ca241972fcee6e2c0e902c1cfa88cfc60cbca7
yujeong <b.imnuj@gmail.com>
2022-08-07 15:30:12 +0900

    refactor: 상품 수정 시 s3에 저장된 기존 이미지 삭제 후 새로운 이미지를 저장하도록 변경

--- src/main/java/shop/tryit/domain/item/service/ImageService.java
+++ src/main/java/shop/tryit/domain/item/service/ImageService.java
@@ -3,7 +3,6 @@ package shop.tryit.domain.item.service;
 import static shop.tryit.domain.item.entity.ImageType.DETAIL;
 import static shop.tryit.domain.item.entity.ImageType.MAIN;
 
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -36,16 +35,14 @@ public class ImageService {
     /**
      * 메인 이미지 수정
      */
-    public Image updateMainImage(Item findItem, ItemRequestDto form) throws IOException {
-        log.info("======== 메인 이미지 수정 시작 ========");
-
+    public Image updateMainImage(Item findItem, ItemRequestDto form) {
         Image findMainImage = findItem.getMainImage(); // 기존 메인 이미지를 찾아옴
 
         log.info("old 메인이미지 = {} , {}", findMainImage.getOriginFileName(), findMainImage.getStoreFileName());
 
         // 새로 넘어온 이미지가 없을 경우 수정 사항 x
         if (!form.getMainImage().isEmpty()) {
-            imageStore.deleteImageFile(findMainImage.getStoreFileName()); // 기존 메인 이미지 삭제
+            imageStore.deleteS3(findMainImage.getStoreFileName()); // 기존 메인 이미지 삭제
             Image newMainImage = uploadMainImage(form); // 새로운 메인 이미지 서버에 저장
             findMainImage.update(newMainImage); // 메인 이미지 정보 업데이트
 
@@ -64,8 +61,7 @@ public class ImageService {
     /**
      * 상세 이미지 수정
      */
-    public Image updateDetailImage(Item item, ItemRequestDto form) throws IOException {
-        log.info("======== 상세 이미지 수정 시작 ========");
+    public Image updateDetailImage(Item item, ItemRequestDto form) {
 
         Image findDetailImage = item.getDetailImage(); // 기존 상세 이미지를 찾아옴
 
@@ -73,7 +69,7 @@ public class ImageService {
 
         // 새로 넘어온 이미지가 있을 경우 기존에 있던 이미지 삭제 후 업로드
         if (!form.getDetailImage().isEmpty()) {
-            imageStore.deleteImageFile(findDetailImage.getStoreFileName()); // 기존 상세 이미지 삭제
+            imageStore.deleteS3(findDetailImage.getStoreFileName()); // 기존 상세 이미지 삭제
             Image newDetailImage = uploadDetailImage(form); // 새로운 상세 이미지 서버에 저장
             findDetailImage.update(newDetailImage); // 상세 이미지 정보 업데이트
 
--- src/main/java/shop/tryit/domain/item/service/ItemFacade.java
+++ src/main/java/shop/tryit/domain/item/service/ItemFacade.java
@@ -1,6 +1,5 @@
 package shop.tryit.domain.item.service;
 
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -58,7 +57,7 @@ public class ItemFacade {
      * 상품 수정
      */
     @Transactional
-    public Long update(Long itemId, ItemRequestDto itemRequestDto) throws IOException {
+    public Long update(Long itemId, ItemRequestDto itemRequestDto) {
         Item findItem = itemService.findItem(itemId);
         Item newItem = toEntity(itemRequestDto);
 
